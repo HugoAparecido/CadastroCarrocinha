d9efaab6ff27c1a02ff09dfd73126e1b311dca10
HugoAparecido <hugoapga626@gmail.com>
2024-07-30 10:35:41 -0300

    Cidade OK

--- includes/conexao.php
+++ includes/conexao.php
@@ -2,7 +2,7 @@
 $hostname = 'localhost';
 $username = 'root';
 $password = '';
-$database = 'IFSP';
+$database = 'CONTROLE_CASTRACAO';
 $port = 3307;
 $con = mysqli_connect($hostname, $username, $password, $database, $port);
 if (mysqli_connect_errno()) {
